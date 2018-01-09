84d77cbdfb4d57c0ead97ba013827e0246d46725
Arian Pasquali <arian@MacBook-Pro-de-Tomas.local>
2018-01-09 06:40:14 +0000

    performance test

--- performance_test.py
+++ performance_test.py
@@ -1,11 +1,15 @@
 import timeit
+import sys
 
-loops = 1000000
+loops = 10000
+
+if(len(sys.argv) > 1):
+	loops = int(sys.argv[1])
 
 setup = "from utils.pagination import Paginator" 
 res = timeit.repeat('Paginator(2147483647,147483647,3,3).paginate()', setup=setup, number=1, repeat=loops)
 
-# timeit in microseconds
+# timeit is measured in microseconds
 # convert it to milliseconds (10**-6)
 
-print("{} loops, best result: {} milliseconds".format(loops, min(res)/(10**-6)))
+print("{0} loops, best result: {1:.15f} milliseconds".format(loops, min(res)/(10**-6)))
